894e03a729376067f057419521751d8a194c8c7f
c1xoot <you@example.com>
2024-12-14 19:23:25 +0300

    functions from router to crud

--- crud.py
+++ crud.py
@@ -1,9 +1,22 @@
+from data_base import fake_database
 from schema import User
 
+def get_all() -> list[User]:
+    return fake_database
+
+def new_user(user: User):
+    fake_database.append(user)
+    return user
 
 def get_by_name(name: str) -> User | None:
-    pass
+    for user in fake_database:
+        if user.first_name.lower() == name.lower():
+            return user
 
-
-def get_all() -> list[User]:
-    pass
+def get_by_row_name(user_name: str, user: User):
+    for row in fake_database:
+        if row.first_name.lower() == user_name.lower():
+            row.first_name = user.first_name
+            row.last_name = user.last_name
+            row.age = user.age
+            return user
--- router.py
+++ router.py
@@ -1,36 +1,29 @@
 from fastapi import APIRouter
-
+from crud import *
 from data_base import fake_database
 from schema import User
 
 router = APIRouter(prefix="/user")
 
 @router.get("/all")
-def get_users() -> list[User]:
-    return fake_database
+def get_users():
+    return get_all()
+
 
 @router.post("/create")
 def add_user(user: User):
-    fake_database.append(user)
-    return user
+    return new_user(user)
+
 
 @router.get("/get_by_name/{name}")
-async def get_user_by_name(name: str):
-    for user in fake_database:
-        if user.first_name.lower() == name.lower():
-            return user
+def get_user_by_name(name: str):
+    return get_by_name(name)
 
 @router.delete("/delite_users")
 def delite_user_by_name(name: str):
-    for user in fake_database:
-        if user.first_name.lower() == name.lower():
-            fake_database.remove(user)
+     fake_database.remove(get_by_name(name))
+
 
 @router.put("/update_last_name/{user_name}")
 def update_last_name(user_name: str, user: User):
-    for row in fake_database:
-        if row.first_name.lower() == user_name.lower():
-            row.first_name = user.first_name
-            row.last_name = user.last_name
-            row.age = user.age
-            return user
+    return get_by_row_name(user_name, user)
