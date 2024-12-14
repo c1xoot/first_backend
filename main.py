67adb3f5607ba5df4b3223760242efb9df80ce2b
c1xoot <you@example.com>
2024-12-14 22:49:44 +0300

    import and include routers

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from routers.user import user_router
+from routers.adress import address_router
 
-import routers.user, routers.adress
 
 app = FastAPI()
 
-app.include_router(routers.user.router)
+app.include_router(address_router)
+app.include_router(user_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=1488)
