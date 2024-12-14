73fba761579b28f14738c1527195be0038a1eb6e
c1xoot <you@example.com>
2024-12-14 22:32:57 +0300

    rename adress router

--- routers/adress.py
+++ routers/adress.py
@@ -3,27 +3,27 @@ from crud.adress import *
 from data_base import adress_table
 from shemas.adress import Adress
 
-router = APIRouter(prefix="/user")
+address_router = APIRouter(prefix="/adress")
 
-@router.get("/all")
+@address_router.get("/all")
 def get_adress():
     return get_all()
 
 
-@router.post("/create")
+@address_router.post("/create")
 def add_adress(adress: Adress):
     return new_adress(adress)
 
 
-@router.get("/get_by_city/{city}")
+@address_router.get("/get_by_city/{city}")
 def get_user_by_city(city: str):
     return get_by_city(city)
 
-@router.delete("/delite_users")
+@address_router.delete("/delite_users")
 def delite_adress_by_city(city: str):
      adress_table.remove(get_by_city(city))
 
 
-@router.put("/update_last_name/{user_name}")
+@address_router.put("/update_last_name/{user_name}")
 def update_adress(city: str, adress: Adress):
     return get_by_row_city(city, adress)
